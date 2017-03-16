7d7e77c608f225830fa61e8147eb9fee5ed6a1cb
Shay Katz <Shayk@fst21.local>
2017-03-16 16:31:29 +0200

    initial code for kubernetes service discovery example via dns

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# kubernetes-examples" 
+#Main target
+Supply a very simple example of kubernetes service discovery using minikube
+# pre-reuiqsit
+minikube installation is ready and running 
+# Getting started 
+eval $(minikube docker-env)
+cd service2
+mvn clean package docker:build
+cd k8s
+kubectl create -f s2
+cd ../../service1
+mvn clean package docker:build
+cd k8s
+
+
+
+
---
+++ service2/src/main/java/hello/Application.java
@@ -0,0 +1,47 @@
+package hello;
+
+import io.fabric8.kubernetes.api.KubernetesHelper;
+import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/hello-service2")
+    public String home() throws IOException {
+        return "Hello from service2";
+    }
+
+    public String getResponse(String url) throws IOException {
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        HttpResponse response = httpClient.execute(request);
+//
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
